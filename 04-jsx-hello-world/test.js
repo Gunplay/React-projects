5c9fb3678e0cebe80f19c40904f8dab4774b2923
Vova Yankers <vovarojko@gmail.com>
2022-11-26 11:32:09 +0200

    Create button App use to React syntax

---
+++ 04-jsx-hello-world/test.js
@@ -0,0 +1,6 @@
+function HelloReact() {
+    return (
+        React.createElement("h1", null, "Hello from React") // не является синтаксисом для js
+        React.createElement("h1", null, "Hello from React")
+        );
+  }
