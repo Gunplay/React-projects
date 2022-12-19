892d41ae406d37ced28a22d03e6a4db675b39159
Vova Yankers <vovarojko@gmail.com>
2022-12-19 21:32:20 +0200

    added func handler and controlled input

--- 24-Todo-app-v1/src/App.js
+++ 24-Todo-app-v1/src/App.js
@@ -4,12 +4,18 @@ import TodoForm from './components/Todos/TodoForm'
 import TodoList from './components/Todos/TodoList'
 
 function App() {
-  const [todos, setTodos] = useState(['todo one', 'todo two'])
+  const [todos, setTodos] = useState([])
   console.log(todos)
+
+  const addTodoHandler = (text) => {
+    // Массив из старых значений + new text
+    setTodos([...todos, text])
+  }
   return (
     <div className="App">
       <h1>Todo App</h1>
-      <TodoForm />
+      {/* Передаем функцию в TodoForm */}
+      <TodoForm addTodo={addTodoHandler} />
       <TodoList todos={todos} />
     </div>
   )
--- 24-Todo-app-v1/src/components/Todos/TodoForm.js
+++ 24-Todo-app-v1/src/components/Todos/TodoForm.js
@@ -3,14 +3,23 @@ import styles from './TodoForm.module.css'
 
 console.log(styles)
 
-function TodoForm() {
+function TodoForm({ addTodo }) {
+  const [text, setText] = useState('')
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+    addTodo(text)
+    setText('')
+  }
   return (
-    <form>
+    <form onSubmit={onSubmitHandler}>
       <input
         className={styles.userText}
         type="text"
         name="input"
         placeholder="Enter new todo"
+        // Controlled input Enter
+        value={text}
+        onChange={(e) => setText(e.target.value)}
       ></input>
       <button className={styles.buttonSubPush} type="Submit">
         Submit
