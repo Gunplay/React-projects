3fe7d5b679342f7def0ffc0f142bb07a29eff771
Vova Yankers <vovarojko@gmail.com>
2023-01-12 11:25:06 +0200

    style

--- 26-react-router/src/components/NotFound.jsx
+++ 26-react-router/src/components/NotFound.jsx
@@ -1,9 +1,8 @@
-import Menu from "./Menu"
-
 export const NotFound = () => {
   return (
     <>
-    <h1>Not Found</h1>
+    <h1 style={{color: 'red'}}>Not Found</h1>
+    <h2 style={{color: 'orange'}}>Something went wrong...</h2>
     </>
   )
 }
